44a18da87541174b9566d04fd41f6ac8386e0cb6
Alumno <Alejandro@gmail.com>
2022-06-20 19:12:46 +0200

    commit dados...

--- app/src/main/java/com/example/dados/MainActivity.kt
+++ app/src/main/java/com/example/dados/MainActivity.kt
@@ -8,6 +8,14 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val dados =  arrayOf(
+        R.drawable.dice1, //entero
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
@@ -16,19 +24,10 @@ class MainActivity : AppCompatActivity() {
     fun dados(view: View){
         val num = Random.nextInt(1,7)
         val num2 = Random.nextInt(1,7)
-        binding.imageView.setImageResource(R.drawable.dice1)
-        binding.imageView2.setImageResource(R.drawable.dice2)
-        val d1 = (1..6).random()
-        val d2 = (1..6).random()
-        val numeros = when (d1) {
-            1->R.drawable.dice1
-            2->R.drawable.dice2
-            3->R.drawable.dice3
-            4->R.drawable.dice4
-            5->R.drawable.dice5
-            else->R.drawable.dice6
+        binding.imageView.setImageResource(dados[num-1])
+        binding.imageView2.setImageResource(dados[num2-1])
+        binding.tvResultado.text = (num+num2).toString()
 
-        }
     }
 }
 
